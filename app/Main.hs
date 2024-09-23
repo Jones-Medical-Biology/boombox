2bcc4d24a98ee3e9b362420e9bce8593d498d2d4
Noah Jones <noahtjones@gmail.com>
2024-09-23 12:07:22 -0400

    got it working

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Lib (giveMeData)
+import System.Environment
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  case args of
+    (x:xs) -> case x of
+      "knn" -> case xs of
+        (x:_) -> giveMeData x
+        _     -> putStrLn "I need a filepath"
+      _     -> putStrLn "I need a program I know (knn)"
+    _      -> putStrLn "I need the name of a program (knn) and an argument (filepath)"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,20 @@
-module Lib where ()
+{-# LANGUAGE ScopedTypeVariables, BangPatterns #-}
 
+module Lib ( giveMeData
+           ) where
 
+import Umap ( kNearestNeighbors
+            , DataPoint)
+
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import Data.Char (ord)
+import qualified Data.Vector as V
+
+giveMeData :: FilePath -> IO ()
+giveMeData x = do
+  csvData      <- BL.readFile x
+  let myDecode = decode NoHeader csvData :: Either String (V.Vector Umap.DataPoint)
+  case myDecode of
+    Left err -> putStrLn err
+    Right vs -> (putStrLn . show) $ kNearestNeighbors (tail (V.toList vs)) (head (V.toList vs))
