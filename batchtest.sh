48dcf7eba8a8f847f8a7b122564751f738852199
Noah Thomas Jones <noahtjones@gmail.com>
2024-09-26 12:19:47 -0400

    for generating and performing tests

---
+++ batchtest.sh
@@ -0,0 +1,43 @@
+sh timegen.sh --haskell -r 1000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 2000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 3000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 4000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 5000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 6000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 8000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 16000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 32000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 64000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 128000 -c 1000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 2000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 3000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 4000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 5000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 6000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 8000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 16000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 32000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 64000 -n 10 \
+    && sh timegen.sh --haskell -r 1000 -c 128000 -n 10
+
+sh timegen.sh --python -r 1000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 2000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 3000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 4000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 5000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 6000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 8000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 16000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 32000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 64000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 128000 -c 1000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 2000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 3000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 4000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 5000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 6000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 8000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 16000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 32000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 64000 -n 10 \
+    && sh timegen.sh --python -r 1000 -c 128000 -n 10
--- timegen.sh
+++ timegen.sh
@@ -1,14 +1,20 @@
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 --haskell|--python -r ROWS -c COLUMNS -n NTESTS"
+    echo "Usage: $0 --haskell|--python -r ROWS -c COLUMNS -n NTESTS [-o FILE]"
     exit 1
 fi
-
+CSV_FILE="time.csv"
 while [[ $# -gt 0 ]]; do
     case $1 in
-	--haskell) lang=haskell ;;
-	--python)  lang=python  ;;
+	--haskell)
+	    lang=haskell
+	    shift 1
+	    ;;
+	--python)
+	    lang=python
+	    shift 1
+	    ;;
 	-r|--rows)
-	    if [[ $# -gt 1 && $2 =~ ^[0-9]+$ ]]; then
+	    if [[ $# -gt 1 && $2 =~ ^[0-9]++$ ]]; then
 		rows=$2
 		shift 2
 	    else
@@ -34,15 +40,23 @@ while [[ $# -gt 0 ]]; do
 		exit 1
 	    fi
 	    ;;
+	-o|--out)
+	    if [[ $# -gt 1 ]]; then
+		CSV_FILE=$2
+		shift 2
+	    else
+		echo "Error: need a filename" >&2
+		exit 1
+	    fi
+	    ;;
+
 	*)
 	    echo "Failed to parse: $1" >&2
 	    exit 1
 	    ;;
     esac
-    shift
 done
 TEST_FILE="r$rows.c$cols.test.csv"
-CSV_FILE="$lang.r$rows.c$cols.time.csv"
 
 if [ ! -f "$TEST_FILE" ]; then
     sh gentestdata.sh $rows $cols $TEST_FILE
@@ -50,22 +64,28 @@ fi
 
 # Create CSV header if file doesn't exist
 if [ ! -f "$CSV_FILE" ]; then
-    echo "RealTime,UserTime,SystemTime" > $CSV_FILE
+    echo "RealTime,UserTime,SystemTime,Rows,Columns,Language" > $CSV_FILE
 fi
 
 case $lang in
     haskell)
 	for ((i=1; i<=ntests; i++)); do
 	    env time -f "%e,%U,%S" cabal exec \
-		 bioboombox knn $TEST_FILE \
-		 >> $CSV_FILE
+		bioboombox knn $TEST_FILE \
+		     2>&1 >/dev/null \
+		| tr -d '\n' \
+		     >> $CSV_FILE
+	    echo ,$rows,$cols,$lang >> $CSV_FILE
 	done
 	;;
     python)
 	for ((i=1; i<=ntests; i++)); do
 	    env time -f "%e,%U,%S" python \
 		nearestneighbors.py $TEST_FILE \
-		>> $CSV_FILE
+		     2>&1 >/dev/null \
+	     	| tr -d '\n' '' \
+		     >> $CSV_FILE
+	    echo ,$rows,$cols,$lang >> $CSV_FILE
 	done
 	;;
 esac	   
